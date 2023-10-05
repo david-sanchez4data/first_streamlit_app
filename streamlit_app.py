9bd72c4582e39d3e8b37dcdebfe43790248fe7b5
david-sanchez4data <147070400+david-sanchez4data@users.noreply.github.com>
2023-10-05 16:33:01 +0200

    Update streamlit_app.py
    Add menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('Nuevo menu Familiar')
+
+streamlit.header(`Breakfast Menu`)
+streamlit.text(`Omega 3 & blueberry Oatmeal`)
+streamlit.text(`Kale, Spinach & Roocket Smoothie`)
+streamlit.text(`Hard-Boiled Free-Range Egg`)
