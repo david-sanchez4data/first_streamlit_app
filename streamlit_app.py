8039e99d3236a01934983b62440177a74708296c
david-sanchez4data <147070400+david-sanchez4data@users.noreply.github.com>
2023-10-05 16:35:02 +0200

    Update streamlit_app.py
    Fix Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('Nuevo menu Familiar')
 
-streamlit.header(`Breakfast Menu`)
-streamlit.text(`Omega 3 & blueberry Oatmeal`)
-streamlit.text(`Kale, Spinach & Roocket Smoothie`)
-streamlit.text(`Hard-Boiled Free-Range Egg`)
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Roocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
